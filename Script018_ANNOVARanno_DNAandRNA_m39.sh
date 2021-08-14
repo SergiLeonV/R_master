dd6814fca23769518cf13a4a4d4429b885c5d51a
SergiLeonV <86607762+SergiLeonV@users.noreply.github.com>
2021-08-14 16:19:53 +0200

    adjust

--- Script018_ANNOVARanno_DNAandRNA_m39.sh
+++ Script018_ANNOVARanno_DNAandRNA_m39.sh
@@ -15,24 +15,24 @@
 cd /home/sleon.1/Software/annovar/annovar
 
 ### PRIMERO GENERAMOS EL AVINPUT PARTIENDO DE NUESTRO VCF FILE. PRIMERO HACEMOS PARA EL ARCHIVO DE SNP L3-L3RR: 
-perl convert2annovar.pl -format vcf4 /home/sleon.1/Proj01/TFM/Raw_data/DNA/DNA_PE_alineadas/Lacun3RR-bam/Lacun3.Lacun3RR.snp.vcf \
--outfile /home/sleon.1/Proj01/TFM/Raw_data/DNA/DNA_PE_alineadas/Lacun3RR-bam/Lacun3.Lacun3RR_snp_fromvcf.avinput -withfreq -includeinfo
+perl convert2annovar.pl -format vcf4 /home/sleon.1/Proj01/TFM/Raw_data/DNA/DNA_PE_alineadas/Lacun3RR-bam/Lacun3.Lacun3RR.snp.Somatic.vcf \
+-outfile /home/sleon.1/Proj01/TFM/Raw_data/DNA/DNA_PE_alineadas/Lacun3RR-bam/Lacun3.Lacun3RR.snp.Somatic.avinput -withfreq -includeinfo
 
 ### A CONTINUACIÓN GENERAMOS EL ARCHIVO ANOTADO Y LO GUARDAMOS EN LA CARPETA DE INTERES.
-perl table_annovar.pl /home/sleon.1/Proj01/TFM/Raw_data/DNA/DNA_PE_alineadas/Lacun3RR-bam/Lacun3.Lacun3RR_snp_fromvcf.avinput mousedb/ -protocol wgEncodeGencodeBasicVM27 -buildver mm39 \
--out /home/sleon.1/Proj01/TFM/Raw_data/DNA/DNA_PE_alineadas/Lacun3RR-bam/Lacun3.Lacun3RR_snp_fromvcf.annotated -operation g -nastring . -polish
+perl table_annovar.pl /home/sleon.1/Proj01/TFM/Raw_data/DNA/DNA_PE_alineadas/Lacun3RR-bam/Lacun3.Lacun3RR.snp.Somatic.avinput mousedb/ -protocol wgEncodeGencodeBasicVM27 -buildver mm39 \
+-out /home/sleon.1/Proj01/TFM/Raw_data/DNA/DNA_PE_alineadas/Lacun3RR-bam/Somatic_SNP_DNA_Lacun3.Lacun3RR.annotated -operation g -nastring . -polish
 
 
 
 ### REPETIMOS EL PROCESO PARA EL ARCHIVO QUE SE HA GENERADO DE INDEL L3-L3RR:
 cd /home/sleon.1/Software/annovar/annovar
 
-perl convert2annovar.pl -format vcf4 /home/sleon.1/Proj01/TFM/Raw_data/DNA/DNA_PE_alineadas/Lacun3RR-bam/Lacun3.Lacun3RR.indel.vcf \
--outfile /home/sleon.1/Proj01/TFM/Raw_data/DNA/DNA_PE_alineadas/Lacun3RR-bam/Lacun3.Lacun3RR_indel_fromvcf.avinput -withfreq -includeinfo
+perl convert2annovar.pl -format vcf4 /home/sleon.1/Proj01/TFM/Raw_data/DNA/DNA_PE_alineadas/Lacun3RR-bam/Lacun3.Lacun3RR.indel.Somatic.vcf \
+-outfile /home/sleon.1/Proj01/TFM/Raw_data/DNA/DNA_PE_alineadas/Lacun3RR-bam/Lacun3.Lacun3RR.indel.Somatic.avinput -withfreq -includeinfo
 
 ### A CONTINUACIÓN GENERAMOS EL ARCHIVO ANOTADO Y LO GUARDAMOS EN LA CARPETA DE INTERES.
-perl table_annovar.pl /home/sleon.1/Proj01/TFM/Raw_data/DNA/DNA_PE_alineadas/Lacun3RR-bam/Lacun3.Lacun3RR_indel_fromvcf.avinput mousedb/ -protocol wgEncodeGencodeBasicVM27 -buildver mm39 \
--out /home/sleon.1/Proj01/TFM/Raw_data/DNA/DNA_PE_alineadas/Lacun3RR-bam/Lacun3.Lacun3RR_indel_fromvcf.annotated -operation g -nastring . -polish
+perl table_annovar.pl /home/sleon.1/Proj01/TFM/Raw_data/DNA/DNA_PE_alineadas/Lacun3RR-bam/Lacun3RR-bam/Lacun3.Lacun3RR.indel.Somatic.avinput mousedb/ -protocol wgEncodeGencodeBasicVM27 -buildver mm39 \
+-out /home/sleon.1/Proj01/TFM/Raw_data/DNA/DNA_PE_alineadas/Lacun3RR-bam/Somatic_INDEL_DNA_Lacun3.Lacun3RR.annotated -operation g -nastring . -polish
 
 #####################################################################
 
@@ -40,24 +40,24 @@ perl table_annovar.pl /home/sleon.1/Proj01/TFM/Raw_data/DNA/DNA_PE_alineadas/Lac
 cd /home/sleon.1/Software/annovar/annovar
 
 ### PRIMERO GENERAMOS EL AVINPUT PARTIENDO DE NUESTRO VCF FILE. PRIMERO HACEMOS PARA EL ARCHIVO DE SNP L3-L3RR (RNA):
-perl convert2annovar.pl -format vcf4 /home/sleon.1/Proj01/results/alignments/L3-RR-1/Lacun3.Lacun3RR.snp.vcf \
--outfile /home/sleon.1/Proj01/results/alignments/L3-RR-1/Lacun3.Lacun3RR_snp_fromvcf.avinput -withfreq -includeinfo
+perl convert2annovar.pl -format vcf4 /home/sleon.1/Proj01/results/alignments/L3-RR-1/RNA_Lacun3.Lacun3RR.snp.Somatic.vcf \
+-outfile /home/sleon.1/Proj01/results/alignments/L3-RR-1/Lacun3.Lacun3RR.snp.Somatic.avinput -withfreq -includeinfo
 
 ### A CONTINUACIÓN GENERAMOS EL ARCHIVO ANOTADO Y LO GUARDAMOS EN LA CARPETA DE INTERES.
-perl table_annovar.pl /home/sleon.1/Proj01/results/alignments/L3-RR-1/Lacun3.Lacun3RR_snp_fromvcf.avinput mousedb/ -protocol wgEncodeGencodeBasicVM27 -buildver mm39 \
--out /home/sleon.1/Proj01/results/alignments/L3-RR-1/Lacun3.LacunRR_RNA_snp_fromvcf.annotated -operation g -nastring . -polish
+perl table_annovar.pl /home/sleon.1/Proj01/results/alignments/L3-RR-1/Lacun3.Lacun3RR.snp.Somatic.avinput mousedb/ -protocol wgEncodeGencodeBasicVM27 -buildver mm39 \
+-out /home/sleon.1/Proj01/results/alignments/L3-RR-1/Somatic_SNP_RNA_Lacun3.Lacun3RR.annotated -operation g -nastring . -polish
 
 
 
 ### REPETIMOS EL PROCESO PARA EL ARCHIVO QUE SE HA GENERADO DE INDEL: 
 cd /home/sleon.1/Software/annovar/annovar
 
-perl convert2annovar.pl -format vcf4 /home/sleon.1/Proj01/results/alignments/L3-RR-1/Lacun3.Lacun3RR.indel.vcf \
--outfile /home/sleon.1/Proj01/results/alignments/L3-RR-1/Lacun3.Lacun3RR_indel_fromvcf.avinput -withfreq -includeinfo
+perl convert2annovar.pl -format vcf4 /home/sleon.1/Proj01/results/alignments/L3-RR-1/RNA_Lacun3.Lacun3RR.indel.Somatic.vcf \
+-outfile /home/sleon.1/Proj01/results/alignments/L3-RR-1/RNA_Lacun3.Lacun3RR.indel.Somatic.avinput -withfreq -includeinfo
 
 ### A CONTINUACIÓN GENERAMOS EL ARCHIVO ANOTADO Y LO GUARDAMOS EN LA CARPETA DE INTERES.
-perl table_annovar.pl /home/sleon.1/Proj01/results/alignments/L3-RR-1/Lacun3.Lacun3RR_indel_fromvcf.avinput mousedb/ -protocol wgEncodeGencodeBasicVM27 -buildver mm39 \
--out /home/sleon.1/Proj01/results/alignments/L3-RR-1/Lacun3.Lacun3RR_RNA_indel_fromvcf.annotated -operation g -nastring . -polish
+perl table_annovar.pl /home/sleon.1/Proj01/results/alignments/L3-RR-1/RNA_Lacun3.Lacun3RR.indel.Somatic.avinput mousedb/ -protocol wgEncodeGencodeBasicVM27 -buildver mm39 \
+-out /home/sleon.1/Proj01/results/alignments/L3-RR-1/Somatic_INDEL_RNA_Lacun3.Lacun3RR.annotated -operation g -nastring . -polish
 
 
 
